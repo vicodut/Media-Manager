572be236df824bae07fd9010a19ed31f4aef4358
ysiguman <victord675@gmail.com>
2014-09-28 11:57:53 +0200

    Add error at findData.js

--- src/js/findData.js
+++ src/js/findData.js
@@ -6,43 +6,46 @@ function findData (title, Tour) {
 	tmdb.search('movie', {query: title, language: 'fr'}, function (err, results) {
 	    //Informations on first ID
 
-	    if (results.results[0] == undefined ) {
-
-	    	data[0] = title;
-	        data[1] = "Data/movie-placeholder.jpg";
-	        data[2] = "N.C.";
-	        data[3] = 'N.C.';
-	        data[4] = "N.C.";
-	        list(data,Tour);
+	    if (err) {
 
 	    } else {
 
-	    tmdb.infos('movie', results.results[0].id, {language: 'fr'}, function (err, results) {
-	    	compteurImg++;
-	    	/*console.log(results);*/
-	        while (data == null) {
-	        };
+		    if (results.results[0] == undefined ) {
 
-	        data[0] = title;
-	        data[1] = results.poster_path;
-	        data[2] = results.overview;
-	        data[3] = Math.round(results.vote_average / 2);
-	        data[4] = results.genres;
+		    	data[0] = title;
+		        data[1] = "Data/movie-placeholder.jpg";
+		        data[2] = "N.C.";
+		        data[3] = 'N.C.';
+		        data[4] = "N.C.";
+		        list(data,Tour);
 
-	        var download = new Download().get({url : results.poster_path, name : title + '.jpg'}, 'Data/Img/');
+		    } else {
 
-	        download.run(function (err, files) {
-			    if (err) {
-			        throw err;
-			    }
-			    console.log(files);
-			    compteurImg--;
-			    console.log(compteurImg);
-			});
+		    tmdb.infos('movie', results.results[0].id, {language: 'fr'}, function (err, results) {
+		    	compteurImg++;
+		    	/*console.log(results);*/
+
+		        data[0] = title;
+		        data[1] = results.poster_path;
+		        data[2] = results.overview;
+		        data[3] = Math.round(results.vote_average / 2);
+		        data[4] = results.genres;
+
+		        var download = new Download().get({url : results.poster_path, name : title + '.jpg'}, 'Data/Img/');
+
+		        download.run(function (err, files) {
+				    if (err) {
+				        throw err;
+				    }
+				    console.log(files);
+				    compteurImg--;
+				    console.log(compteurImg);
+				});
 
 
-	        list(data,Tour);
-	    });}
+		        list(data,Tour);
+		    });}
+		}
 	});
 	
 }
